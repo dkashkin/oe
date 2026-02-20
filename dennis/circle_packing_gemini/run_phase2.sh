b7c18af343b7dd0716b1c78492a7b050c7753901
dkashkin <kashkin@google.com>
2026-02-20 16:39:28 -0600

    as

--- dennis/circle_packing_gemini/run_phase2.sh
+++ dennis/circle_packing_gemini/run_phase2.sh
@@ -2,4 +2,4 @@
 python openevolve-run.py dennis/circle_packing_gemini/openevolve_output/checkpoints/checkpoint_100/best_program.py \
   dennis/circle_packing_gemini/evaluator.py \
   --config dennis/circle_packing_gemini/config_phase_2.yaml \
-  --iterations 100
+  --iterations 10
