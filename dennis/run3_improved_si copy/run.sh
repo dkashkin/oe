3dbdf7e0a86a977e17ad03e99aa10cca3fcbb9c5
dkashkin <kashkin@google.com>
2026-02-23 14:41:10 -0600

    3

--- dennis/run3_improved_si copy/run.sh
+++ dennis/run3_improved_si copy/run.sh
@@ -1,5 +1,5 @@
 # Phase 1: Initial exploration
-python openevolve-run.py dennis/run2_improved_si/initial_program.py \
-  dennis/run2_improved_si/evaluator.py \
-  --config dennis/run2_improved_si/config.yaml \
+python openevolve-run.py dennis/run3_improved_si/initial_program.py \
+  dennis/run3_improved_si/evaluator.py \
+  --config dennis/run3_improved_si/config.yaml \
   --iterations 100
