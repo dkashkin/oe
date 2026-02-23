87edb4eaa2fce78b6f3b9f05911d31593bc2e233
dkashkin <kashkin@google.com>
2026-02-23 11:23:38 -0600

    exploration

---
+++ dennis/cp_1model_1phase/run_phase1.sh
@@ -0,0 +1,5 @@
+# Phase 1: Initial exploration
+python openevolve-run.py dennis/cp_1model_1phase/initial_program.py \
+  dennis/cp_1model_1phase/evaluator.py \
+  --config dennis/cp_1model_1phase/config_phase_1.yaml \
+  --iterations 100
