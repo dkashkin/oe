bbe5caff445c9a6acc005581ab3227bdbc499376
dkashkin <kashkin@google.com>
2026-02-23 12:42:46 -0600

    rename2

---
+++ dennis/run2_improved_si/run.sh
@@ -0,0 +1,5 @@
+# Phase 1: Initial exploration
+python openevolve-run.py dennis/run2_improved_si/initial_program.py \
+  dennis/run2_improved_si/evaluator.py \
+  --config dennis/run2_improved_si/config.yaml \
+  --iterations 100
--- dennis/run2_improved_si/run_phase1_v2.sh
+++
@@ -1,5 +0,0 @@
-# Phase 1: Initial exploration
-python openevolve-run.py dennis/cp_1model_1phase/initial_program.py \
-  dennis/cp_1model_1phase/evaluator.py \
-  --config dennis/cp_1model_1phase/config_phase_1_v2.yaml \
-  --iterations 100
