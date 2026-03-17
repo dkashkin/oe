3ee837e95d1d59af66cc0fad5f18a09484025cc1
dkashkin <kashkin@google.com>
2026-03-17 12:13:04 -0500

    a

--- dennis_sort_cpp/run4/run.sh
+++ dennis_sort_cpp/run4/run.sh
@@ -1,4 +1,4 @@
 python openevolve-run.py dennis_sort_cpp/run4/initial_program.cpp \
   dennis_sort_cpp/run4/evaluator.py \
   --config dennis_sort_cpp/run4/config.yaml \
-  --iterations 150
+  --iterations 100
